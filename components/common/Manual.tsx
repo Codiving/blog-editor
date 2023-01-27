92cf154c32fa230e8173f7e4654529c991bd4593
codiving <codivingmoon@gmail.com>
2023-01-27 18:53:12 +0900

    Add Manual component

---
+++ components/common/Manual.tsx
@@ -0,0 +1,12 @@
+const Manual = () => {
+  return (
+    <div>
+      <p style={{ margin: 0, marginBottom: 6 }}># 단축기</p>
+      <span>줄바꿈 추가 : crtl + shift + Enter</span>
+      <br />
+      <br />
+    </div>
+  );
+};
+
+export default Manual;
