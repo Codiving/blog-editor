824015b4b97eb092a5c5e748673ce8e0bb0b57dd
codiving <codivingmoon@gmail.com>
2023-01-27 19:07:12 +0900

    Update Manual

--- components/common/Manual.tsx
+++ components/common/Manual.tsx
@@ -1,11 +1,26 @@
+import styled from "@emotion/styled";
+
+const Container = styled("div")(() => ({
+  marginBottom: 10
+}));
+
+const Title = styled("p")(() => ({
+  fontWeight: "bold",
+  margin: 0
+}));
+
+const Content = styled("p")(() => ({
+  margin: 0,
+  fontSize: 14
+}));
+
 const Manual = () => {
   return (
-    <div>
-      <p style={{ margin: 0, marginBottom: 6 }}># 단축기</p>
-      <span>줄바꿈 추가 : crtl + shift + Enter</span>
-      <br />
-      <br />
-    </div>
+    <Container>
+      <Title># 단축기</Title>
+      <Content>- 줄바꿈 추가 : crtl + shift + Enter</Content>
+      <Content>- 다음 칸 이동 추가 : Enter</Content>
+    </Container>
   );
 };
 
